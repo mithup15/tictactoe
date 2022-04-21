99e4cab5c2932c342d2d3508f8635b4a2cad0e50
mithup15 <96760114+mithup15@users.noreply.github.com>
2022-04-21 16:49:58 +0530

    Delete src/main/java/tic directory

--- src/main/java/tic/Evaluation.java
+++
@@ -1,8 +0,0 @@
-package tic;
-
-public enum Evaluation {
-	  NOWINNER, /* valid board, but the game hasn't been won by either player yet */
-	  XWINS, /* valid board, and the game was won by player X */
-	  OWINS, /* valid board, and the game was won by player O */
-	  UnreachableState /* string describes a board, but not a board state that is reachable from a valid game */
-	}
